bf938daae4551147e07c5019500bc3a3ae770484
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-06 11:03:31 -0600

    Externalize App component and import it instead

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,5 @@
-// Entry point for the build script in your package.json
-import '@hotwired/turbo-rails'
-import './controllers'
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-function App() {
-  return <h1>Hello World!</h1>
-}
+import { App } from './pages/App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
