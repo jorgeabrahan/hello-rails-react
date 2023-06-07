b4b47ac76831a2d01220f5b070a060c0b28ada4e
jorgeabrahan <jorge24abrahan@gmail.com>
2023-06-06 19:58:05 -0600

    Fix rubocop linter errors

--- app/controllers/greeting_controller.rb
+++ app/controllers/greeting_controller.rb
@@ -1,8 +1,8 @@
 class GreetingController < ApplicationController
   def random_greeting
     if Greeting.exists?
-      greeting = Greeting.order("RANDOM()").first
-      render json: { greeting: greeting }
+      greeting = Greeting.order('RANDOM()').first
+      render json: { greeting: }
     else
       render json: {}
     end
